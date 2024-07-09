e4de06c02a42bdbbf4968ea51acca24888f284ff
Fabio Alves <faedicao@gmail.com>
2024-07-09 10:20:27 -0300

    Atividade Finalizada

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(palavraEFrase) {
+
+    
+    const texto = palavraEFrase.replace(/\s/g, '').toLowerCase()
+    const tamanho = texto.length
+  
+    for (let i = 0; i < tamanho; i++) {
+        if (texto[i] !== texto[tamanho - 1 - i]) {
+            return false 
+        }
+    }
+  
+   
+    return true 
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+
+    let minimo = arr[0] 
+
+    let maximo = arr[0] 
+
+    for (let i = 1; i < arr.length; i++) {
+
+        if (arr[i] < minimo) minimo = arr[i]
+
+        if (arr[i] > maximo) maximo = arr[i]
+    }
+
+    
+    return [minimo, maximo] 
 }
